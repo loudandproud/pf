036770c1cfe72d635a4bff49d692f94c023bdc2d
Gazettemag <tech@gazettemag.com>
2015-11-11 11:25:00 -0800

    Make a new file; gazettemag.php

---
+++ gazettemag.php
@@ -0,0 +1,94 @@
+<div id="gazettemag" class="panel" data-number="4">
+        <a href="#portfolio" data-number="3" class="nav backto-pf" id="backto">
+            <img src="img/arrow-l.png"  alt="back-to-about" class="backto-pf-img">
+        </a>
+        <a href="#contact" data-number="10" class="nav goto-contact" id="goto">
+            <img src="img/arrow-r.png"  alt="go-to-contact" class="goto-contact-img">
+        </a>
+        <section class="page-sec cf">
+            <div class="page-inner-top">
+                <h1 class="page-h1 gzt-h1">Gazette Magazine</h1>
+                <h3 class="page-headdescription">Core Concept, Magazine Layout, Deep customized Wordpress theme</h3>
+                <p class="description-p">Gazette Magazine is a local art, culture, and news magazine. The magazine started from Gastown two years ago. It featured local events, underground arts, music, fashion, and people living in Gastown.
+                The magazine successfully published first hard-copy version this January, and the second issue will come out soon.
+                </p>
+                <p class="tools-p">HTML5, CSS3, SASS, jQuery, PHP, MySQL, Photoshop, Illustrator</p>
+            </div>
+        </section>
+        <section class="page-sec">
+           <div class="page-inner">
+                <img src="img/gazettemag/gazettemag-home.jpg" alt="ubc-wireframe" class="ubc-home3d">
+           </div>
+        </section>
+        <section class="page-sec gzt-b1">
+            <div class="page-inner">
+                <div class="cf gzt-homewf">
+                    <div class="wfdescription gzt-cards-sec">
+                        <h2 class="page-h2 gzt-h2">It started from concept making</h2>
+                        <p>The first thing that I realized was lacking of concept. I believe inspiring people by their concept is essential for a medium company,
+                        so an enthusiastic email explaining the importance of having a concept was sent to everyone. <br>
+                        Talents gathered, and a brainstorming meeting was taken place.
+                        After two intensive meetings, a strap line that our thoughts condensed emerged; <br> <i>&ldquo;Evolve together.&rdquo;</i></p>
+                    </div>
+                    <img src="img/gazettemag/cardtest.png" class="gzt-cards">
+                </div>
+            </div>
+        </section>
+        <section class="page-sec gzt-b2">
+            <div class="page-inner">
+                <div class="cf gzt-homewf">
+                    <img src="img/gazettemag/pc-home.jpg" class="gzt-left">
+                    <div class="wfdescription gzt-right">
+                        <h2 class="page-h2 gzt-h2">It dramatically shows featured images</h2>
+                        <p>The website featured high quality images. The images should intrigue readers rather than just an attached to posts.
+                        <br>The grid-system layout shows images dynamically and dramatically.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-sec gzt-b3">
+            <div class="page-inner">
+                <div class="cf gzt-homewf">
+                    <div class="wfdescription gzt-left">
+                        <h2 class="page-h2 gzt-h2">Doubled ad-qlicks</h2>
+                        <p>Banners are not only ads, it also gives the layout rhythm. The ad revenue is a key engines of medium business. Advertising should be, yet it matches the design.</p>
+                        <img src="img/gazettemag/sp-list.jpg"  alt="" class="gzt-list-sp">
+                    </div>
+                    <img src="img/gazettemag/pc-list2.jpg" class="gzt-right">
+                </div>
+                
+            </div>
+        </section>
+        <section class="page-sec cf">
+            <div class="page-inner">
+
+                <div class="ubc-mobile tip cf" title="Hover <br>The <br>Devices" >
+                    <div class="page-iphone6" id="offtip1">
+                        <div class="page-iphone6-inner">    
+                            <img src="img/gazettemag/sp-post.jpg" alt="gazettemag-smartphone-post" class="gzt-sp-post">
+                        </div>
+                    </div>
+                    <div class="gzt-circulation">
+                        <h2 class="page-h2 gzt-h2">Circulation matters</h2>
+                        <p class="ubc-sps-p">Users can smoothly move to next post by scrolling.
+                        Related posts are placed under the post and on the sidebar of desktop.
+
+                        That enhances user&prime;s circulation.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>    
+        <section class="page-sec cf tip">
+            <div class="page-inner">
+                <h2 class="page-h2 ">What they had to say...</h2>
+                <div class="tstmnl cf">
+                    <img src="img/gazettemag/dani.jpg" alt="gazette-magazine-testmimonial" class="tstmnl-left">
+                    <p class="tstmnl-right">Toru is second to none.</p>
+                    <span class="tstmnl-spn">Dani Kremeniuk, Art Director</span>
+                </div>
+            </div>
+        </section>    
+    <a href="https://gazettemag.com" class="viewsite" target="_blank">View Site</a>
+</div><!-- Gazettemag -->
