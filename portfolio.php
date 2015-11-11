6df4404ad8625d7bd224b96929d25ff59d1560da
Gazettemag <tech@gazettemag.com>
2015-11-11 14:37:02 -0800

    Add gazette magazine

--- portfolio.php
+++ portfolio.php
@@ -1,85 +1,98 @@
-<div id="portfolio" class="panel" data-number="3">
-    <div class="portfolio">
-        <a href="#contact" data-number="10" class="nav goto-contact  pf-goto-contact" id="goto">
-            <img src="img/arrow-rw.png"  alt="go-to-contact" class="goto-contact-img" id="goto-contact-img">
-        </a>
-        <section class="portfolio-ubc" id="portfolio-ubc" data-background="http://toruinamoto.com/img/ubcbaja/ubctop-bg.jpg">
-            <div class="cf left-ubc">
-                <h1 class="portfolio-h1">UBC BAJA Website Contest</h1>
-                <h3 class="portfolio-h3">Uuniversity of British Columbia Racing Car Club&rsquo;s Webdesign</h3>
-                <i class="portfolio-i">Interface Design &amp; HTML&amp;CSS Coding</i>
-                <img src="img/winner.png" alt="winner-banner" class="ubc-winner">
-                <a href="#ubcbaja" data-number="4" class="nav pf-next-w pf-next-ubc">
-                   View Project
-                </a>
-            </div>
-            <div class="ubc-top-img cf">
-            <img src="img/ubcbaja/ubc-top-pc.jpg" alt="ubcbaja-top-pc" class="ubc-top-pc">
-            <img src="img/ubcbaja/ubc-top-ph.png" alt="ubcbaja-top-phone" class="ubc-top-ph">
-            </div>
-        </section>
-        <section class="portfolio-nwp" id="portfolio-nwp" data-background="http://toruinamoto.com/img/newsportal/nwptop-bg.jpg">
-            <div class="cf left-nwp">
-                <h1 class="portfolio-h1">Direct Marketing News</h1>
-                <h3 class="portfolio-h3">Design,Custom CMS</h3>
-                <i class="portfolio-i">Interface Design &amp; Frontend/Backend Development</i>
-                <a href="#newsportal" data-number="5" class="nav pf-next-w pf-next-nwp">
-                   View Project
-                </a>
-            </div>    
-            <img src="img/newsportal/nwp-top.png" alt="newspoprtal" class="pf-right nwp-topimg">
-        </section>
-        <section class="portfolio-ecm" id="portfolio-ecm" data-background="http://toruinamoto.com/img/ecommerce/ecmtop-bg.jpg">
-            <div class="cf left-ecm">
-                <h1 class="portfolio-h1">SNOW FREAK</h1>
-                <h3 class="portfolio-h3">E-commerce</h3>
-                <i class="portfolio-i">User Interface Design</i>
-                <a href="#ecommerce" data-number="6" class="nav pf-next pf-next-ecm">
-                   View Project
-                </a>
-            </div>    
-            <img src="img/ecommerce/ecm-top.png" alt="ecommerce" class="pf-right ecm-topimg">
-        </section>
-        <section class="portfolio-rgk cf" id="portfolio-rgk" data-background="">
-            <div class="left-rgk">
-                <div class="rgkleft-inner">
-                    <h1 class="portfolio-h1">Reggie <span class="rgkh1-c1">Key </span><span class="rgkh1-c2">Watson</span></h1>
-                    <h3 class="portfolio-h3">Branding Case Study</h3>
-                    <i class="portfolio-i">Interface Design &amp; Frontend/Backend Development</i>
-                    <a href="#reggiekey" data-number="7" class="nav pf-next-w pf-next-rgk">
+    <div id="portfolio" class="panel" data-number="3">
+        <div class="portfolio">
+            <a href="#contact" data-number="11" class="nav goto-contact  pf-goto-contact" id="goto">
+                <img src="img/arrow-rw.png"  alt="go-to-contact" class="goto-contact-img" id="goto-contact-img">
+            </a>
+            <section class="portfolio-gzt" id="portfolio-gzt" data-background="http://toruinamoto.com/img/gazettemag/gazettemag-bg.jpg">
+                <div class="cf left-ubc">
+                    <h1 class="portfolio-h1">Gazette Magazine</h1>
+                    <h3 class="portfolio-h3">Art &amp; Culture Web magazine design renewal</h3>
+                    <i class="portfolio-i">Interface Design &amp; HTML&amp;CSS coding, jQuery, PHP programming, custom Wordpress theme</i>
+                    <a href="#gazettemag" data-number="4" class="nav pf-next-w pf-next-gzt">
                        View Project
                     </a>
                 </div>
-            </div>
-            <div class="middle-rgk">   
-                <img src="img/reggiekeyswatson/rgk-top.jpg" alt="newspoprtal" class="rgk-topimg">
-            </div>
-        </section>
-        <section class="portfolio-wzb tip cf" title="Hover <br>The <br>Phone" id="portfolio-wzb" data-background="http://toruinamoto.com/img/wazabibuzz/wzbtop-bg.jpg">
-            <div class="cf left-wzb">
-                <h1 class="portfolio-h1">WaZabiBuzz</h1>
-                <h3 class="portfolio-h3">Mobile First Design, Wordpress Integration</h3>
-                <i class="portfolio-i">Interface Design &amp; Frontend/Wordpress Development</i>
-                <a href="#wazabibuzz" data-number="8" class="nav pf-next pf-next-wzb">
-                   View Project
-                </a>
-            </div>
-            <div class="wzb-imgwrp">
-                <div class="wzb-top" id="offtip1">
-                    <div class="wzb-top-inner">    
-                        <img src="img/wazabibuzz/wzb-top.jpg" alt="wazabibuzz-topimage" class="wzb-topimg">
+                <div class="ubc-top-img cf">
+                <!-- <img src="img/gazettemag/gzt-top.jpg" alt="ubcbaja-top-pc" class="ubc-top-pc"> -->
+                </div>
+            </section>
+            <section class="portfolio-ubc" id="portfolio-ubc" data-background="http://toruinamoto.com/img/ubcbaja/ubctop-bg.jpg">
+                <div class="cf left-ubc">
+                    <h1 class="portfolio-h1">UBC BAJA Website Contest</h1>
+                    <h3 class="portfolio-h3">Uuniversity of British Columbia Racing Car Club&rsquo;s Webdesign</h3>
+                    <i class="portfolio-i">Interface Design &amp; HTML&amp;CSS Coding</i>
+                    <img src="img/winner.png" alt="winner-banner" class="ubc-winner">
+                    <a href="#ubcbaja" data-number="5" class="nav pf-next-w pf-next-ubc">
+                       View Project
+                    </a>
+                </div>
+                <div class="ubc-top-img cf">
+                <img src="img/ubcbaja/ubc-top-pc.jpg" alt="ubcbaja-top-pc" class="ubc-top-pc">
+                <img src="img/ubcbaja/ubc-top-ph.png" alt="ubcbaja-top-phone" class="ubc-top-ph">
+                </div>
+            </section>
+            <section class="portfolio-nwp" id="portfolio-nwp" data-background="http://toruinamoto.com/img/newsportal/nwptop-bg.jpg">
+                <div class="cf left-nwp">
+                    <h1 class="portfolio-h1">Direct Marketing News</h1>
+                    <h3 class="portfolio-h3">Design,Custom CMS</h3>
+                    <i class="portfolio-i">Interface Design &amp; Frontend/Backend Development</i>
+                    <a href="#newsportal" data-number="6" class="nav pf-next-w pf-next-nwp">
+                       View Project
+                    </a>
+                </div>    
+                <img src="img/newsportal/nwp-top.png" alt="newspoprtal" class="pf-right nwp-topimg">
+            </section>
+            <section class="portfolio-ecm" id="portfolio-ecm" data-background="http://toruinamoto.com/img/ecommerce/ecmtop-bg.jpg">
+                <div class="cf left-ecm">
+                    <h1 class="portfolio-h1">Snow Freak</h1>
+                    <h3 class="portfolio-h3">E-commerce</h3>
+                    <i class="portfolio-i">User Interface Design</i>
+                    <a href="#ecommerce" data-number="7" class="nav pf-next pf-next-ecm">
+                       View Project
+                    </a>
+                </div>    
+                <img src="img/ecommerce/ecm-top.png" alt="ecommerce" class="pf-right ecm-topimg">
+            </section>
+            <section class="portfolio-rgk cf" id="portfolio-rgk" data-background="">
+                <div class="left-rgk">
+                    <div class="rgkleft-inner">
+                        <h1 class="portfolio-h1">Reggie <span class="rgkh1-c1">Key </span><span class="rgkh1-c2">Watson</span></h1>
+                        <h3 class="portfolio-h3">Branding Case Study</h3>
+                        <i class="portfolio-i">Interface Design &amp; Frontend/Backend Development</i>
+                        <a href="#reggiekey" data-number="8" class="nav pf-next-w pf-next-rgk">
+                           View Project
+                        </a>
                     </div>
                 </div>
-            </div>
-        </section>        
-        <section class="portfolio-eas" id="portfolio-eas" data-background="http://toruinamoto.com/img/eastasianserif/eastop-bg.jpg">
-            <div class="cf eas-inner">
-                <h1 class="portfolio-h1">East Asian Serif</h1>
-                <h3 class="portfolio-h3">Original Typeface</h3>
-                <a href="#typography" data-number="9" class="nav pf-next-w pf-next-eas">
-                   View Project
-                </a>
-            </div>            
-        </section>
-    </div><!--portfolio-->
-</div>
+                <div class="middle-rgk">   
+                    <img src="img/reggiekeyswatson/rgk-top.jpg" alt="newspoprtal" class="rgk-topimg">
+                </div>
+            </section>
+            <section class="portfolio-wzb tip cf" title="Hover <br>The <br>Phone" id="portfolio-wzb" data-background="http://toruinamoto.com/img/wazabibuzz/wzbtop-bg.jpg">
+                <div class="cf left-wzb">
+                    <h1 class="portfolio-h1">WaZabiBuzz</h1>
+                    <h3 class="portfolio-h3">Mobile First Design, Wordpress Integration</h3>
+                    <i class="portfolio-i">Interface Design &amp; Frontend/Wordpress Development</i>
+                    <a href="#wazabibuzz" data-number="9" class="nav pf-next pf-next-wzb">
+                       View Project
+                    </a>
+                </div>
+                <div class="wzb-imgwrp">
+                    <div class="wzb-top" id="offtip1">
+                        <div class="wzb-top-inner">    
+                            <img src="img/wazabibuzz/wzb-top.jpg" alt="wazabibuzz-topimage" class="wzb-topimg">
+                        </div>
+                    </div>
+                </div>
+            </section>        
+            <section class="portfolio-eas" id="portfolio-eas" data-background="http://toruinamoto.com/img/eastasianserif/eastop-bg.jpg">
+                <div class="cf eas-inner">
+                    <h1 class="portfolio-h1">East Asian Serif</h1>
+                    <h3 class="portfolio-h3">Original Typeface</h3>
+                    <a href="#typography" data-number="10" class="nav pf-next-w pf-next-eas">
+                       View Project
+                    </a>
+                </div>            
+            </section>
+        </div><!--portfolio-->
+    </div>
